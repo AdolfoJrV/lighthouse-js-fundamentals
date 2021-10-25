2ebe5cbbac6aa9fb4869b46ed583859cb710f408
AdolfoJrV <adolfojr.villegas@edu.sait.ca>
2021-10-25 13:49:44 +0000

    Add xMarksThePerfectShot.js file

---
+++ xMarksThePerfectShot.js
@@ -0,0 +1,50 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+
+const finalPosition = function (moves) {
+  let xCoordinate = 0;
+  let yCoordinate = 0;
+  let finalCoordinates = [xCoordinate, yCoordinate];
+
+  for (let move of moves) {
+    switch (move) {
+      case 'north':
+        yCoordinate += 1;
+        break;
+      case 'east':
+        xCoordinate += 1;
+        break;
+      case 'south':
+        yCoordinate -= 1;
+        break;
+      case 'west':
+        xCoordinate -= 1;
+        break;
+    }
+    finalCoordinates[0] = xCoordinate;
+    finalCoordinates[1] = yCoordinate;
+  }
+  return finalCoordinates;
+}
+
+// const finalPosition = function (moves) {
+//   let xCoordinate = 0;
+//   let yCoordinate = 0;
+//   let finalCoordinates = [0, 0];
+
+//   for (let move of moves) {
+//     if (move === 'north') {
+//       yCoordinate += 1;
+//     } else if (move === 'east') {
+//       xCoordinate += 1;
+//     } else if (move === 'south') {
+//       yCoordinate -= 1;
+//     } else if (move === 'west') {
+//       xCoordinate -= 1;
+//     }
+//   }
+//   finalCoordinates[0] = xCoordinate;
+//   finalCoordinates[1] = yCoordinate;
+//   return finalCoordinates;
+// }
+
+console.log(finalPosition(moves));
