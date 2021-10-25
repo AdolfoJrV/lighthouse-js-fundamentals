fabf847b6ccb06ec66a4d501029b4549213865db
AdolfoJrV <adolfojr.villegas@edu.sait.ca>
2021-10-25 12:31:30 +0000

    Modify the votingStationCalculation.js code to be more readable

--- votingStationCalculation.js
+++ votingStationCalculation.js
@@ -4,10 +4,28 @@ const stations = [
   ['Moose Mountain Community Centre', 45, 'community centre']
 ];
 
+// const chooseStations = function (stations) {
+//   let selectedStations = [];
+//   for (let station of stations) {
+//     station[1] >= 20 && (station[2] === 'school' || station[2] === 'community centre') ? selectedStations.push(station[0]) : null;
+//   }
+//   return selectedStations;
+// }
+
+// console.log(chooseStations(stations));
+
 const chooseStations = function (stations) {
+// Declaring more readable variables.
   let selectedStations = [];
+  let stationName;
+  let stationCapacity;
+  let stationType;
   for (let station of stations) {
-    station[1] >= 20 && (station[2] === 'school' || station[2] === 'community centre') ? selectedStations.push(station[0]): null; 
+    // Initializing the variables with the corresponding array index.
+    stationName = station[0];
+    stationCapacity = station[1];
+    stationType = station[2];
+    stationCapacity >= 20 && (stationType === 'school' || stationType === 'community centre') ? selectedStations.push(stationName) : null;
   }
   return selectedStations;
 }
